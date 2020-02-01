d6f6b5cac656b70719947ef9fe60ac3fafc98956
Nick Nguyen <ptnguyen1901@gmail.com>
2020-01-31 20:56:50 -0500

    Counter app

---
+++ Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Nick Nguyen on 1/31/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
